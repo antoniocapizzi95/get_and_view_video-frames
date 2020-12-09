8759b1248ca61bffdee8e609593da24bb9b45659
Antonio Capizzi <antoniocapizzi95@hotmail.it>
2020-12-10 00:07:24 +0100

    first commit

---
+++ collect_frames.py
@@ -0,0 +1,32 @@
+import pafy
+import cv2
+from os import path
+
+def collect_frames(yt_url, interval=10, directory_path = ""):
+    vPafy = pafy.new(yt_url)
+    play = vPafy.getbest(ftypestrict=False, preftype="webm")
+
+    # start the video
+    cap = cv2.VideoCapture(play.url)
+    i = 0
+    count = 1
+
+    success = True
+    fps = int(cap.get(cv2.CAP_PROP_FPS))
+    while success:
+        success, frame = cap.read()
+        if i % (interval * fps) == 0:
+            try:
+                if not path.exists(directory_path+"frame" + str(count) + ".jpg"):
+                    cv2.imwrite(directory_path+'frame%d.jpg' % count, frame)
+                    print('successfully written one frame')
+                count += 1
+            except:
+                print("error")
+        i += 1
+
+        if cv2.waitKey(20) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
---
+++ main.py
@@ -0,0 +1,27 @@
+import collect_frames
+import threading
+from flask import Flask
+from flask_cors import CORS
+from flask import send_file
+
+
+app = Flask(__name__)
+CORS(app)
+
+directory = ""
+def collect_thread():
+    url = 'https://www.youtube.com/watch?v=AmrrSfiMxGA'
+    collect_frames.collect_frames(url, directory_path=directory)
+
+
+@app.route("/<index>", methods=['GET'])
+def get_frame(index):
+
+    return send_file(directory+"frame"+index+".jpg", mimetype='image/jpg')
+
+
+
+if __name__ == '__main__':
+    collect_th = threading.Thread(target=collect_thread)
+    collect_th.start()
+    app.run(port=3000)
