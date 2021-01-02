d51a46b53268b3fd96169a24d567cdedb41c15d4
antoniocapizzi95 <antoniocapizzi95@hotmail.it>
2021-01-02 23:10:51 +0100

    tolto run.sh e sostituito con build.sh

--- run.sh
+++
@@ -1,4 +0,0 @@
-sudo docker build -t get_frames:latest get_frames/.
-sudo docker build -t view_frames:latest view_frames/.
-sudo docker run -d --name get_frames --privileged -v /home/images:/home/images get_frames:latest
-sudo docker run -d -p 3000:3000 --name view_frames --privileged -v /home/images:/home/images view_frames:latest
