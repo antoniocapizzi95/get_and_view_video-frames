d8336d62cea0268c5c40aa18d795a718aff7bd0a
antoniocapizzi95 <antoniocapizzi95@hotmail.it>
2021-01-02 19:25:30 +0100

    Update collect_frames.py

--- get_frames/collect_frames.py
+++ get_frames/collect_frames.py
@@ -3,7 +3,7 @@ import cv2
 from os import path
 
 yt_url = 'https://www.youtube.com/watch?v=AmrrSfiMxGA'
-interval = 30
+interval = 10
 directory_path = "/home/images/"
 #def collect_frames(yt_url, interval=10, directory_path = ""):
 vPafy = pafy.new(yt_url)
